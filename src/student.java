0d755bb9d2a75fe9aa63aa990e8d02f753acde05
Amen Endriyas <amenendriyas@gmail.com>
2026-06-17 17:04:23 +0300

    first commit

---
+++ src/student.java
@@ -0,0 +1,17 @@
+public class student {
+    private String name;
+   private int studentid;
+   private String department;
+   private double GPA;
+    
+   public student(int studentid, String name, String department, double GPA){ 
+     this.GPA = GPA;
+     this.department = department;
+     this.name = name;
+     this.studentid = studentid;
+}
+ public String getStudentid() {
+        return tudentid;
+    }
+
+}
